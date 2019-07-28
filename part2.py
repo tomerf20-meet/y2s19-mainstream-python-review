7b41ce766d41543d7b44baa52cd235580c75b88c
myname21-meet <myname21@meet.mit.edu>
2019-07-28 09:15:40 -0400

    started python1 lab

--- part2.py
+++ part2.py
@@ -1,7 +1,21 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
+	while not isPrime(x):
+		x+=1
+	while not isPrime(y):
+		y+=1
+	if 1 < y < 250 and 500 < x < 1000:
+			return x*y
+	else:
+		print ("invalid input: outside range")
+
 
 def decode(coded_message):
 	pass
+
+def isPrime(num):
+	for i in range(2,num):
+		if (num % i) == 0:
+			return False
+	return True
